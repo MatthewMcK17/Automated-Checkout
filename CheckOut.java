efed325c5a5a42c88fc81a16acec19f3def5a53f
Matthew McKeever <51388006+MatthewMcK17@users.noreply.github.com>
2020-12-18 23:41:15 -0500

    Update CheckOut.java

--- CheckOut.java
+++ CheckOut.java
@@ -8,12 +8,13 @@ public class CheckOut {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		Random rand = new Random();
+		APS aps;
 		
 		System.out.print("Please enter your name: ");
 		String name = in.nextLine();
 		System.out.println();
 		
-		APS aps = new APS(rand.nextInt(999999999), name);
+		aps = new APS(rand.nextInt(999999999), name);
 		
 		// Choose 3 for credit card type and use the # 379354508162306
 		aps.createCart(in);
